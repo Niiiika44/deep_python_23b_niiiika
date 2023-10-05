66a6461efb474888837ca2e3cc14eb8528628989
nika <nika.burdelnaya@mail.ru>
2023-10-05 14:00:07 +0300

    fix: default keywords and required_fields

--- 02/src/first_json_callback.py
+++ 02/src/first_json_callback.py
@@ -3,7 +3,7 @@ import re
 
 
 def keyword_callback(key: str, keyword: str):
-    print(f'{key}: {keyword}')
+    return f'{key}: {keyword}'
 
 
 def parse_json(json_str: str,
@@ -13,17 +13,25 @@ def parse_json(json_str: str,
 
     if not isinstance(json_str, str):
         raise TypeError("Wrong value for json_str")
-    if not isinstance(keywords, list):
+    if not (isinstance(keywords, list) or keywords is None):
         raise TypeError("Wrong value for keywords")
-    if not isinstance(required_fields, list):
+    if not (isinstance(required_fields, list) or required_fields is None):
         raise TypeError("Wrong value for required_fields")
     if not callable(callback):
         raise TypeError("keyword_callback is not a func")
 
-    json_doc = json.loads(json_str)
+    if keywords is None or required_fields is None:
+        return
+
+    try:
+        json_doc = json.loads(json_str)
+    except json.decoder.JSONDecodeError:
+        print("json_str should be a json-file of the str type")
+        raise
+
     for field in required_fields:
         if field in json_doc:
             for k in keywords:
                 pattern = fr'\b{k}\b'
-                if re.search(pattern, str(json_doc[field]), flags=re.I):
-                    callback(field, k)
+                if res := re.search(pattern, str(json_doc[field]), flags=re.I):
+                    callback(field, res[0])
