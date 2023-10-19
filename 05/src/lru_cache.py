52e812fdefad8a2616914c5c12e48f679b949bca
nika <nika.burdelnaya@mail.ru>
2023-10-19 22:02:52 +0300

    fix: dct -> nonpublic attr

--- 05/src/lru_cache.py
+++ 05/src/lru_cache.py
@@ -1,9 +1,9 @@
 class LRUCache:
     '''LRU (Least Recently Used) Cache w/out OrderedDict'''
-    __slots__ = ("dct", "limit")
+    __slots__ = ("_dct", "limit")
 
     def __init__(self, limit=42):
-        self.dct = {}
+        self._dct = {}
         self.limit = limit
 
     def __setattr__(self, name, value):
@@ -15,18 +15,18 @@ class LRUCache:
         return super().__setattr__(name, value)
 
     def get(self, key):
-        res = self.dct.get(key, None)
+        res = self._dct.get(key, None)
         if res is not None:
-            self.dct.pop(key)
-            self.dct[key] = res
+            self._dct.pop(key)
+            self._dct[key] = res
         return res
 
     def set(self, key, value):
         if key.__hash__ is None:
             raise TypeError("key should be hashable")
-        if key not in self.dct and len(self.dct) == self.limit:
-            self.dct.pop(list(self.dct.keys())[0])
-        self.dct[key] = value
+        if key not in self._dct and len(self._dct) == self.limit:
+            self._dct.pop(list(self._dct.keys())[0])
+        self._dct[key] = value
 
     def __getitem__(self, key):
         return self.get(key)
